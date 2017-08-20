2216ff67b21ad7e33b6292875f729f5300faf377
DaBigJoe <dabigjoe97@gmail.com>
2017-08-20 18:34:56 +0100

    Updated API to 0.2.1

--- src/main/java/apiTutorial/entity/RenderSaiga.java
+++ src/main/java/apiTutorial/entity/RenderSaiga.java
@@ -1,7 +1,5 @@
 package apiTutorial.entity;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.util.ResourceLocation;
 import obsidianAPI.render.RenderAnimated;
 
 public class RenderSaiga extends RenderAnimated
